a984f576dc3a8fc5377095c8957866d059e51584
Kodek81 <contact.kodek81@gmail.com>
2013-09-22 01:24:01 -0700

    Implementados los modos sin corchetes y sin paréntesis.

--- Phonegap_Combinadas/assets/www/js/index.js
+++ Phonegap_Combinadas/assets/www/js/index.js
@@ -9,6 +9,7 @@ var hayPar = false; //Si el corchete contiene un paréntesis cerrado
 var corchUtil = false; //Si el corchete contiene un paréntesis cerrado y un signo
 
 var parAb = false; //Si un paréntesis ha sido abierto y no cerrado
+var parIni = false; //Si un paréntesis se ha abierto al principio
 var haySigno = false; //Si el paréntesis contiene un signo
 
 var contPar = false; //Si la operación contiene un paréntesis
@@ -19,9 +20,9 @@ var contDiv = false; //Si la operación contiene una división
 var caracUlt = parseInt("0"); //Cuál ha sido el último caracter introducido
 //1: [ , 2: ( , 3: número , 4: signo , 5: ) , 6: ]
 
-var modoCD = false; //Modo de juego con divisiones
-var modoCP = false; //Modo de juego con paréntesis
-var modoCC = false; //Modo de juego con corchetes
+var modoCD = true; //Modo de juego con divisiones
+var modoCP = true; //Modo de juego con paréntesis
+var modoCC = true; //Modo de juego con corchetes
 
 
 //EVENTOS AL INICIO
@@ -33,6 +34,9 @@ window.addEventListener('load', function() {
 
 $('#inicioPage').bind('pagebeforeshow', function(event) {
     reset();
+    $('#checkbox-div').attr("checked", true).checkboxradio("refresh");
+    $('#checkbox-par').attr("checked", true).checkboxradio("refresh");
+    $('#checkbox-cor').attr("checked", true).checkboxradio("refresh");
 });
 
 $('#combinadasPage').bind('pageshow', function(event) {
@@ -48,10 +52,16 @@ $('#checkbox-div').bind ("change", function (event) {
 
 $('#checkbox-par').bind ("change", function (event) {
 	modoCP = $("#checkbox-par").is(":checked");
+	if (modoCP == false && $("#checkbox-cor").is(":checked") == true) {
+		$('#checkbox-cor').attr("checked", false).checkboxradio("refresh");
+	}
 });
 
 $('#checkbox-cor').bind ("change", function (event) {
 	modoCC = $("#checkbox-cor").is(":checked");
+	if (modoCC == true && $("#checkbox-par").is(":checked") == false) {
+		$('#checkbox-par').attr("checked", true).checkboxradio("refresh");
+	}
 });
 
 $('#botEmpezar').bind('vclick', function(event) { 
@@ -70,17 +80,55 @@ $('#botOperacion').bind('vclick', function(event) {
 //FUNCIONES
 
 function getOperacion(){
-	primerCaracter();
-	cuerpoOperacion();
-	ultimoCaracter();
-	
-	//Última comprobación para ver si la operación aporta algo
-	if (contCorch == false || contPar == false || contMult == false && contDiv == false){
-	   reset();
-	   getOperacion();
-	   return;    
+	if (modoCC == true && modoCP == true) { //Modo corchetes y paréntesis
+		
+		primerCaracter();
+		cuerpoOperacion();
+		ultimoCaracter();
+		
+		//Última comprobación para ver si la operación aporta algo
+		if (contCorch == false || contPar == false || contMult == false && contDiv == false){
+			reset();
+			getOperacion();
+			return;    
+		} else if (modoCD == true && contDiv == false) {
+			reset();
+			getOperacion();
+			return;
+		}
+	} else if (modoCP == true) { //Modo paréntesis sin corchetes
+		
+		primerCaracterSC();
+		cuerpoOperacionSC();
+		ultimoCaracterSC();
+		
+		//Última comprobación para ver si la operación aporta algo
+		if (contPar == false || contMult == false && contDiv == false){
+			reset();
+			getOperacion();
+			return;    
+		} else if (modoCD == true && contDiv == false) {
+			reset();
+			getOperacion();
+			return;
+		}
+	} else { //Modo sin corchetes ni paréntesis
+		
+		primerCaracterSP();
+		cuerpoOperacionSP();
+		
+		if (contMult == false && contDiv == false){
+			reset();
+			getOperacion();
+			return;    
+		} else if (modoCD == true && contDiv == false) {
+			reset();
+			getOperacion();
+			return;
+		}
 	}
 	
+	//Esto es común para todos los modos
 	var operacionLimpia = operacion.split("*").join("x");	
 	$("#boxOperacion").html(operacionLimpia);
 	
@@ -119,11 +167,34 @@ function primerCaracter() {
 	}
 }
 
+function primerCaracterSC() {
+	var num = Math.random();
+	if (num < 0.3){
+		operacion += "( ";
+		caracUlt = 2;
+		parAb = true;
+		parIni = true;
+		haySigno = false;
+	} else {
+		operacion += getNum();
+		caracUlt = 3;
+		longAct++;
+	}
+}
+
+function primerCaracterSP() {
+	operacion += getNum();
+	caracUlt = 3;
+	longAct++;
+}
+
 
 function cuerpoOperacion() {
 	
 	while(longAct < longMax) {
 		
+		//Si hay un corchete abierto con un paréntesis abierto, fuerza su cierre en el 
+		//penúltimo caracter para que el corchete pueda cerrarse siendo útil
 		if ( (longAct + 1) == longMax && corchUtil == false && parAb == true) {
 			if (caracUlt == 2 || haySigno == false){
 				reset();
@@ -145,7 +216,7 @@ function cuerpoOperacion() {
 		}
 		
 		switch(caracUlt) {
-			case 1:
+			case 1: //  [
 				var num = Math.random();
 				if (num > 0 && num < 0.3 && (longMax - longAct) > 1){
 					operacion += "( ";
@@ -159,13 +230,13 @@ function cuerpoOperacion() {
 				}
 				break;
 				
-			case 2:
+			case 2: //  (
 				operacion += getNum();
 				caracUlt = 3;
 				longAct++;
 				break;
 				
-			case 3:
+			case 3: //  número
 				if (parAb == true && haySigno == true) {
 					var num = Math.random();
 					if (num > 0 && num < 0.2){
@@ -210,7 +281,7 @@ function cuerpoOperacion() {
 				}
 				break;
 				
-			case 4:
+			case 4: //  signo
 				if (corchAb == true && parAb == true) {
 					operacion += getNum();
 					caracUlt = 3;
@@ -255,7 +326,7 @@ function cuerpoOperacion() {
 				}
 				break;
 			
-			case 5:
+			case 5: //  )
 				if (corchAb == true && corchUtil == true) {
 					var num = Math.random();
 					if (num > 0 && num < 0.5){
@@ -274,7 +345,7 @@ function cuerpoOperacion() {
 				}
 				break;
 			
-			case 6:
+			case 6: //  ]
 				operacion += getSigno();
 				caracUlt = 4;
 				break;
@@ -282,6 +353,92 @@ function cuerpoOperacion() {
 		} //Cierre del switch
 	} //Cierre del while
 } //Cierre de cuerpoOperacion()
+
+function cuerpoOperacionSC() {
+	
+	while(longAct < longMax) {
+		
+		switch(caracUlt) {
+				
+			case 2: // (
+				operacion += getNum();
+				caracUlt = 3;
+				longAct++;
+				break;
+				
+			case 3: //  número
+				if (parAb == true && haySigno == true) {
+					var num = Math.random();
+					if (num > 0 && num < 0.3){
+						operacion += ") ";
+						caracUlt = 5;
+						parAb = false;
+						haySigno = false;
+						contPar = true;
+					} else {
+						operacion += getSigno();
+						caracUlt = 4;
+					}
+				} else if (parAb == true && haySigno == false) {
+					operacion += getSigno();
+					caracUlt = 4;
+					haySigno = true;
+				} else {
+					operacion += getSigno();
+					caracUlt = 4;
+				}
+				break;
+				
+			case 4: //  signo
+				if (parAb == false) { //Así no se abren paréntesis dentro de paréntesis
+					var num = Math.random();
+					if (num < 0.3) {
+						operacion += "( ";
+						caracUlt = 2;
+						parAb = true;
+						haySigno = false;
+					} else {
+						operacion += getNum();
+						caracUlt = 3;
+						longAct++;
+					}
+				} else {
+					operacion += getNum();
+					caracUlt = 3;
+					longAct++;
+				}
+				break;
+			
+			case 5: //  )
+				operacion += getSigno();
+				caracUlt = 4;
+				break;
+			
+		} //Cierre del switch
+	} //Cierre del while
+} //Cierre de cuerpoOperacionSC()
+
+function cuerpoOperacionSP() {
+	
+	while(longAct < longMax) {
+		
+		switch(caracUlt) {
+							
+			case 3: //  número
+				operacion += getSigno();
+				caracUlt = 4;
+				break;
+				
+			case 4: //  signo
+				operacion += getNum();
+				caracUlt = 3;
+				longAct++;
+				break;
+						
+		} //Cierre del switch
+	} //Cierre del while
+} //Cierre de cuerpoOperacionSP()
+
 
 function ultimoCaracter(){
 	if (parAb == true && corchAb == true){
@@ -312,6 +469,21 @@ function ultimoCaracter(){
 	}
 }
 
+function ultimoCaracterSC(){
+	if (parAb == true) {
+		if (parIni == true) {
+			reset();
+			getOperacion();
+			return;
+		} else if (haySigno == false) {
+			reset();
+			getOperacion();
+			return;
+		}
+		operacion += ") ";
+		contPar = true;
+	}
+}
 
 function getNum() {
 	var numero = Math.ceil(Math.random()*9);
@@ -357,6 +529,7 @@ function reset() {
 	haySigno = false;
 	hayPar = false;
 	corchIni = false;
+	parIni = false;
 	contCorch = false;
 	contPar = false;
 	contMult = false;
